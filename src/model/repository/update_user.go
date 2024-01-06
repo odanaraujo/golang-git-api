013b2c005902372fd923df322ad420e6685e8437
root <root@DESKTOP-5AVCFE7>
2024-01-05 22:41:59 -0300

    update user in the all layers

--- src/model/repository/update_user.go
+++ src/model/repository/update_user.go
@@ -6,26 +6,29 @@ import (
 	"github.com/odanaraujo/golang/users-api/src/configuration/logger"
 	"github.com/odanaraujo/golang/users-api/src/model"
 	"github.com/odanaraujo/golang/users-api/src/model/repository/entity/converter"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.uber.org/zap"
 	"os"
 )
 
-func (ur *userRepository) CreateUser(userDomain model.UserDomainInterface) (model.UserDomainInterface, *exception.Exception) {
-	logger.Info("init createUser repository", zap.String("Journey", "CreateUser"))
+func (ur *userRepository) UpdateUser(id primitive.ObjectID, userDomain model.UserDomainInterface) *exception.Exception {
+	logger.Info("init UpdateUser repository", zap.String("Journey", "UpdateUser"))
 
+	ctx := context.Background()
 	collectionName := os.Getenv(MONGODB_USER_COLLECTION)
-
 	collection := ur.databaseConnection.Collection(collectionName)
 
 	value := converter.ConverterDomainToEntity(userDomain)
-	result, err := collection.InsertOne(context.Background(), value)
+
+	filter := bson.D{{Key: "_id", Value: id}}
+	update := bson.D{{Key: "$set", Value: value}}
+
+	_, err := collection.UpdateOne(ctx, filter, update)
 
 	if err != nil {
-		return nil, exception.InternalServerException(err.Error())
+		return exception.InternalServerException(err.Error())
 	}
 
-	value.ID = result.InsertedID.(primitive.ObjectID)
-
-	return converter.ConverterEntitytoDomain(*value), nil
+	return nil
 }
--- src/model/service/user_interface.go
+++ src/model/service/user_interface.go
@@ -4,6 +4,7 @@ import (
 	"github.com/odanaraujo/golang/users-api/src/configuration/exception"
 	"github.com/odanaraujo/golang/users-api/src/model"
 	"github.com/odanaraujo/golang/users-api/src/model/repository"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func NewUserDomainService(userRepository repository.UserRepository) UserDomainService {
@@ -17,7 +18,7 @@ type userDomainService struct {
 
 type UserDomainService interface {
 	CreateUser(user model.UserDomainInterface) (model.UserDomainInterface, *exception.Exception)
-	UpdateUser(string, model.UserDomainInterface) *exception.Exception
+	UpdateUser(primitive.ObjectID, model.UserDomainInterface) *exception.Exception
 	FindUserByID(id string) (model.UserDomainInterface, *exception.Exception)
 	DeleteUser(id string) *exception.Exception
 	FindUserByEmail(string) (model.UserDomainInterface, *exception.Exception)
